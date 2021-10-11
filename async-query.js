dd416be741274026f8018a78d8ce10411c44cf61
mahdi.aslami <mahdi.aslami@partsoftware.com>
2021-10-11 12:10:53 +0330

    Added async-query.js and its tests. and refactor it.

---
+++ async-query.js
@@ -0,0 +1,50 @@
+import { where, and, or } from './condition.js'
+
+function query(promise) {
+    return {
+        promise: promise,
+        _filter: null,
+
+        get() {
+            return this.do(data => data)
+        },
+
+        first() {
+            return this.do(data => data[0])
+        },
+
+        last() {
+            return this.do(data => data[data.length - 1])
+        },
+
+        do(callback) {
+            return this.promise.then(this.filter()).then(callback)
+        },
+
+        filter() {
+            if (this._filter) {
+                return data => data.filter(this._filter)
+            }
+
+            return data => data
+        },
+
+        where(column) {
+            return where(column, this, and(this.getFilter()))
+        },
+
+        orWhere(column) {
+            return where(column, this, or(this.getFilter()))
+        },
+
+        setFilter(filter) {
+            this._filter = filter
+        },
+
+        getFilter() {
+            return this._filter ?? (() => true)
+        }
+    }
+}
+
+export default query
