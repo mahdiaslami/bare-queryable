8ee43ed72ccb05808970e75d8a5e2865a9658ea4
mahdi.aslami <mahdi.aslami@partsoftware.com>
2021-10-11 12:24:00 +0330

    Override query instead of duplicatin methods.

--- async-query.js
+++ async-query.js
@@ -1,49 +1,14 @@
-import { where, and, or } from './condition.js'
+import query from './query.js'
 
 function asyncQuery(promise) {
     return {
+        ...query([]),
+
         promise: promise,
-        _filter: null,
-
-        get() {
-            return this.do(data => data)
-        },
-
-        first() {
-            return this.do(data => data[0])
-        },
-
-        last() {
-            return this.do(data => data[data.length - 1])
-        },
 
         do(callback) {
             return this.promise.then(this.filter()).then(callback)
         },
-
-        filter() {
-            if (this._filter) {
-                return data => data.filter(this._filter)
-            }
-
-            return data => data
-        },
-
-        where(column) {
-            return where(column, this, and(this.getFilter()))
-        },
-
-        orWhere(column) {
-            return where(column, this, or(this.getFilter()))
-        },
-
-        setFilter(filter) {
-            this._filter = filter
-        },
-
-        getFilter() {
-            return this._filter ?? (() => true)
-        }
     }
 }
 
