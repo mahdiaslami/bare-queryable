dab564909dd8003ce548f1833b0c9be861f75f5a
mahdyaslami <mahdyaslami@gmail.com>
2021-10-20 14:30:34 +0330

    Added order by for asyncQuery().

--- async-query.test.js
+++ async-query.test.js
@@ -1,4 +1,5 @@
 import query from './async-query'
+import { NUMBER_COMPARATOR, STRING_COMPARATOR } from './query'
 import { data } from './fake.js'
 
 const promise = new Promise((resolve) => resolve(data))
@@ -35,5 +36,16 @@ test('query with condition', () => {
         .where('id').equal(0)
         .get().then(result => {
             expect(result).toEqual([data[0]])
+        })
+})
+
+test('order by two column', () => {
+    expect.assertions(1)
+
+    query(promise)
+        .orderBy('strval2', STRING_COMPARATOR).asc()
+        .orderBy('intval', NUMBER_COMPARATOR).desc()
+        .get().then(result => {
+            expect(result).toEqual([data[3], data[1], data[0], data[2]])
         })
 })
