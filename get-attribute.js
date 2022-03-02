adf5929ef9920d8421ab8b52bd01b794084ba05e
mahdyaslami <mahdyaslami@gmail.com>
2022-03-02 19:14:50 +0330

    makeGetterFunction get 4 level deep keys.

--- get-attribute.js
+++ get-attribute.js
@@ -1,5 +1,18 @@
-function makeGetterFunction(key) {
-    return (obj) => obj[key]
+function makeGetterFunction(pathToKey) {
+    const keys = pathToKey.split('.')
+
+    switch (keys.length) {
+    case 1:
+        return (obj) => obj[keys[0]]
+    case 2:
+        return (obj) => obj[keys[0]][keys[1]]
+    case 3:
+        return (obj) => obj[keys[0]][keys[1]][keys[2]]
+    case 4:
+        return (obj) => obj[keys[0]][keys[1]][keys[2]][keys[3]]
+    default:
+        throw new Error('path to key is too long.')
+    }
 }
 
 export default makeGetterFunction
--- get-attribute.test.js
+++ get-attribute.test.js
@@ -9,3 +9,45 @@ test('get value of key in level one', () => {
 
     expect(getId(obj)).toEqual(1)
 })
+
+test('get value of key in level two', () => {
+    const obj = {
+        prop: {
+            id: 1,
+        },
+    }
+
+    const getId = makeGetterFunction('prop.id')
+
+    expect(getId(obj)).toEqual(1)
+})
+
+test('get value of key in level three', () => {
+    const obj = {
+        prop: {
+            prop: {
+                id: 1,
+            },
+        },
+    }
+
+    const getId = makeGetterFunction('prop.prop.id')
+
+    expect(getId(obj)).toEqual(1)
+})
+
+test('get value of key in level four', () => {
+    const obj = {
+        prop: {
+            prop: {
+                prop: {
+                    id: 1,
+                },
+            },
+        },
+    }
+
+    const getId = makeGetterFunction('prop.prop.prop.id')
+
+    expect(getId(obj)).toEqual(1)
+})
