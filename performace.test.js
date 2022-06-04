6581a1cbc8a4bb1a43e10313e98840c78baf0b6e
mahdyaslami <mahdyaslami@gmail.com>
2022-06-04 16:21:28 +0430

    Update performance.test.js.

--- performace.test.js
+++ performace.test.js
@@ -1,23 +1,38 @@
 /* eslint-disable newline-per-chained-call */
 import { query } from './index.js'
-import { NUMBER_COMPARATOR, STRING_COMPARATOR } from './src/comparators.js'
+import { DATE_COMPARATOR, NUMBER_COMPARATOR, STRING_COMPARATOR } from './src/comparators.js'
 
-test('query on 10000 obj be less than 50ms', () => {
-    const array = factory(10000)
+describe('query on 10000 object', () => {
+    test('4 where conditions should take less then 50ms', () => {
+        const array = factory(10000)
 
-    const start = now()
-    query(array)
-        .where('id').above(50)
-        .where('id').below(9950)
-        .orWhere('id').in([2, 3, 5, 7])
-        .where('name').contain(6)
-        .orderBy('name', STRING_COMPARATOR).asc()
-        .orderBy('id', NUMBER_COMPARATOR).desc()
-        .get()
+        const start = now()
+        query(array)
+            .where('id').above(50)
+            .where('id').below(9950)
+            .orWhere('id').in([2, 3, 5, 7])
+            .orWhere('name').contain(6)
+            .get()
 
-    const end = now()
+        const end = now()
 
-    expect(end - start).toBeLessThan(50)
+        expect(end - start).toBeLessThan(50)
+    })
+
+    test('4 order by should take less than 100ms', () => {
+        const array = factory(10000)
+
+        const start = now()
+        query(array)
+            .orderBy('age', NUMBER_COMPARATOR).asc()
+            .orderBy('name', STRING_COMPARATOR).asc()
+            .orderBy('created_at', DATE_COMPARATOR).asc()
+            .orderBy('id', STRING_COMPARATOR).asc()
+            .get()
+        const end = now()
+
+        expect(end - start).toBeLessThan(100)
+    })
 })
 
 function factory(count) {
@@ -27,10 +42,16 @@ function factory(count) {
         array.push({
             id: i,
             name: hash(),
+            age: rand(50),
+            created_at: now(),
         })
     }
 
     return array
+}
+
+function rand(max) {
+    return Math.floor(Math.random() * max)
 }
 
 function now() {
