e3fd1c172446853fe188247a618f6153109526c8
mahdyaslami <mahdyaslami@gmail.com>
2022-06-04 16:29:07 +0430

    Move join tests to join.test.js.

---
+++ src/join.test.js
@@ -0,0 +1,39 @@
+import query from './query.js'
+import {
+    users, parents,
+} from './fake.js'
+
+test('cross join two arrays', () => {
+    const result = query(users).crossJoin(parents).get()
+
+    const expectedResult = [
+        { ...users[0], ...parents[0] },
+        { ...users[0], ...parents[1] },
+        { ...users[1], ...parents[0] },
+        { ...users[1], ...parents[1] },
+        { ...users[2], ...parents[0] },
+        { ...users[2], ...parents[1] },
+        { ...users[3], ...parents[0] },
+        { ...users[3], ...parents[1] },
+    ]
+
+    expect(result).toEqual(
+        expectedResult,
+    )
+})
+
+test('inner join two arrays on two column', () => {
+    const result = query(users)
+        .join(parents).on('id').equal('parent_id')
+        .get()
+
+    const expectedResult = [
+        { ...users[0], ...parents[0] },
+        { ...users[1], ...parents[0] },
+        { ...users[2], ...parents[1] },
+    ]
+
+    expect(result).toEqual(
+        expectedResult,
+    )
+})
