f7ac5dae51707db44cfe69b80fe75ead66b5fd60
mahdyaslami <mahdyaslami@gmail.com>
2022-06-16 08:55:14 +0430

    Refactor join.

--- src/join.js
+++ src/join.js
@@ -17,79 +17,96 @@ export function on(returnValue) {
 }
 
 export function crossJoin(leftRows, rightRows) {
-    return leftRows.reduce((previous, leftRow) => {
-        rightRows.forEach((rightRow) => {
-            previous.push({
-                ...leftRow,
-                ...rightRow,
-            })
-        })
-
-        return previous
-    }, [])
+    return join(leftRows, rightRows).call()
 }
 
 export function innerJoin(leftRows, rightRows, onExpression) {
-    return leftRows.reduce((previous, leftRow) => {
-        rightRows.forEach((rightRow) => {
-            if (onExpression.call(leftRow, rightRow)) {
-                previous.push({
-                    ...leftRow,
-                    ...rightRow,
-                })
-            }
-        })
-
-        return previous
-    }, [])
+    return join(leftRows, rightRows).setOn(onExpression).call()
 }
 
 export function leftJoin(leftRows, rightRows, onExpression) {
-    return leftRows.reduce((previous, leftRow) => {
-        let hold = true
-
-        rightRows.forEach((rightRow) => {
-            if (onExpression.call(leftRow, rightRow)) {
-                previous.push({
-                    ...leftRow,
-                    ...rightRow,
-                })
-
-                hold = false
-            }
-        })
-
-        if (hold) {
-            previous.push({
-                ...leftRow,
-            })
-        }
-
-        return previous
-    }, [])
+    return join(leftRows, rightRows).setOn(onExpression).holdLeft().call()
 }
 
 export function rightJoin(leftRows, rightRows, onExpression) {
-    return rightRows.reduce((previous, rightRow) => {
-        let hold = true
+    return join(leftRows, rightRows).setOn(onExpression).holdRight().call()
+}
 
-        leftRows.forEach((leftRow) => {
-            if (onExpression.call(leftRow, rightRow)) {
-                previous.push({
-                    ...leftRow,
-                    ...rightRow,
+const Side = {
+    LEFT: 1,
+    RIGHT: 2,
+}
+
+function join(leftRows, rightRows) {
+    return {
+        _nearRows: leftRows,
+        _farRows: rightRows,
+        _onExpression: trueOn(),
+        _hold: false,
+
+        setOn(expression) {
+            this._onExpression = expression
+
+            return this
+        },
+
+        holdLeft() {
+            this._hold = Side.LEFT
+
+            return this
+        },
+
+        holdRight() {
+            this._hold = Side.RIGHT
+
+            const temp = this._nearRows
+            this._nearRows = this._farRows
+            this._farRows = temp
+
+            return this
+        },
+
+        call() {
+            const result = []
+
+            this._nearRows.forEach((nearRow) => {
+                let holded = nearRow
+
+                this._farRows.forEach((farRow) => {
+                    let leftRow = nearRow
+                    let rightRow = farRow
+
+                    if (this._hold === Side.RIGHT) {
+                        leftRow = farRow
+                        rightRow = nearRow
+                    }
+
+                    if (this._onExpression.call(leftRow, rightRow)) {
+                        result.push({
+                            ...leftRow,
+                            ...rightRow,
+                        })
+
+                        holded = false
+                    }
                 })
 
-                hold = false
-            }
-        })
-
-        if (hold) {
-            previous.push({
-                ...rightRow,
+                if (this._hold && holded) {
+                    result.push({
+                        ...holded,
+                    })
+                }
             })
-        }
 
-        return previous
-    }, [])
+            return result
+        },
+    }
+}
+
+function trueOn() {
+    return {
+        call() {
+            return true
+        },
+    }
 }
